a6c074e404b510ccf83f4699eb43978ced4fccc5
EndriitHoda <endrit.hoda@student.uni-pr.edu>
2022-04-11 23:00:38 +0200

    Pjesa e encoder dhe decoder ne Java

---
+++ eclipse-workspace/Base64-Gr16/src/Base64/Base64_Decoder.java
@@ -0,0 +1,10 @@
+package Base64;
+
+public class Base64_Decoder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ eclipse-workspace/Base64-Gr16/src/Base64/Base64_Encoder.java
@@ -0,0 +1,26 @@
+package Base64;
+
+import java.util.Base64;
+
+public class Base64_Encoder {
+
+	public static void main(String[] args) {
+		 // write a sample String to encode
+        String sample = "ELon";
+ 
+        // print actual String
+        System.out.println("Sample String:\n"
+                           + sample);
+ 
+        // Encode into Base64 format
+        String BasicBase64format
+            = Base64.getEncoder()
+                  .encodeToString(sample.getBytes());
+ 
+        // print encoded String
+        System.out.println("Encoded String:\n"
+                           + BasicBase64format);
+
+	}
+
+}
